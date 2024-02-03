95145707d5a6a31ed0ee0a0d371ad1ddfdfd24e3
Evgeniy <121779561+NightRunnerEB@users.noreply.github.com>
2024-02-03 23:20:08 +0300

    Update Homework.c

--- HomeWork3/Homework.c
+++ HomeWork3/Homework.c
@@ -2,30 +2,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fibonacci(int num) {
+int fibonacci_plus(int num) {
     if (num <= 1) {
         return num;
     } else {
-        return fibonacci(num - 1) + fibonacci(num - 2);
+        return fibonacci_plus(num - 1) + fibonacci_plus(num - 2);
+    }
+}
+
+int fibonacci_minus(int num) {
+    if (num >= -1) {
+        return num;
+    } else {
+        return fibonacci_minus(num + 2) - fibonacci_minus(num + 1);
     }
 }
 
 int main(int argc, char *argv[]) {
     int num = atoi(argv[1]);
     pid_t chpid;
+    int result;
     chpid = fork();
     if (chpid == 0) {
-        int count = num;
-        int result = num;
-        while(count > 1) {
-            count -= 1;
-            result = result * count;
+        if(num < 0) {
+            printf("I can't calculate the factorial of a negative number:(");
+        } else if(num == 0){
+            printf("Factorial 0 = 1\n");
+        } else {
+            int count = num;
+            result = num;
+            while(count > 1) {
+                count -= 1;
+                result = result * count;
+            }
+            printf("Factorial %d = %d\n", num, result);
         }
-        printf("Factorial %d = %d\n", num, result);
     } else if (chpid == -1) {
         printf("Error");
     } else {
-        int result = fibonacci(num);
+        if(num > 0) {
+            result = fibonacci_plus(num);
+        } else {
+            result = fibonacci_minus(num);
+        }
         printf("The Fibonacci number for %d is %d\n", num, result);
     }
     return 0;
